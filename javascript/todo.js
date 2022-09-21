f34680a64e7f035ba7b75b0690d5cf2d3a51c506
Isntitfun <112168128+Isntitfun@users.noreply.github.com>
2022-09-21 23:02:04 +0900

    Update todo.js

--- javascript/todo.js
+++ javascript/todo.js
@@ -2,32 +2,59 @@ const todoForm = document.querySelector(".todo-form");
 const todoInput = document.querySelector(".todo-input");
 const todoList = document.querySelector(".todo-list");
 
-const todoArray = [];
-
-function deleteTodoItem(event) {
-  event.target.parentElement.remove();
-}
+let todoArray = [];
 
 function saveTodoArray(todoArray) {
-  localStorage.setItem("Todos", todoArray);
+  localStorage.setItem("Todos", JSON.stringify(todoArray));
 }
 
-function createTodoItem(event) {
-  event.preventDefault();
-  const newTodo = todoInput.value;
-  todoArray.push(newTodo);
+function deleteTodoObj(event) {
+  const deleteTarget = event.target.parentElement;
+  const updatedTodoArray = todoArray.filter(
+    (todo) => todo.id !== parseInt(deleteTarget.id)
+  );
+  todoArray = updatedTodoArray;
   saveTodoArray(todoArray);
+  deleteTarget.remove();
+}
+
+function paintNewTodoObj(newTodoObj) {
   const todoItem = document.createElement("li");
   const todoContent = document.createElement("span");
   const todoDeleteBtnNew = document.createElement("button");
-  todoItem.id = Date.now();
-  todoContent.innerText = newTodo;
+
+  todoContent.innerText = newTodoObj.text;
+  todoItem.id = newTodoObj.id;
   todoDeleteBtnNew.innerText = "X";
-  todoDeleteBtnNew.addEventListener("click", deleteTodoItem);
+
+  todoDeleteBtnNew.addEventListener("click", deleteTodoObj);
+
   todoList.appendChild(todoItem);
   todoItem.appendChild(todoContent);
   todoItem.appendChild(todoDeleteBtnNew);
+
   todoInput.value = "";
 }
 
-todoForm.addEventListener("submit", createTodoItem);
+function createNewTodoObj(newTodo) {
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  todoArray.push(newTodoObj);
+  saveTodoArray(todoArray);
+  paintNewTodoObj(newTodoObj);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const newTodo = todoInput.value;
+  createNewTodoObj(newTodo);
+}
+todoForm.addEventListener("submit", handleSubmit);
+
+const savedTodoArray = JSON.parse(localStorage.getItem("Todos"));
+
+if (savedTodoArray !== null) {
+  savedTodoArray.forEach(paintNewTodoObj);
+}
