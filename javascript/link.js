086786383ebf22cc23f294f412c266af727a2724
Isntitfun <112168128+Isntitfun@users.noreply.github.com>
2022-09-22 20:24:08 +0900

    style__4

---
+++ javascript/link.js
@@ -0,0 +1,15 @@
+const linkBtn = document.querySelector(".link-btn");
+const links = document.querySelector(".links");
+const column1 = document.querySelector(".portal-column1");
+const column2 = document.querySelector(".portal-column2");
+const email = links.querySelector("span:last-child");
+
+function handleClickLinkBtn() {
+  links.classList.toggle("links__linkbtn");
+  column1.classList.toggle("column1__linkbtn");
+  linkBtn.classList.toggle("link-btn__linkbtn");
+  email.select();
+  document.execCommand("copy");
+}
+
+linkBtn.addEventListener("click", handleClickLinkBtn);
