643cae30f955b09a6412a189dac4a09cc9e5f436
Isntitfun <112168128+Isntitfun@users.noreply.github.com>
2022-09-22 20:27:01 +0900

    style__5

--- javascript/link.js
+++ javascript/link.js
@@ -2,7 +2,7 @@ const linkBtn = document.querySelector(".link-btn");
 const links = document.querySelector(".links");
 const column1 = document.querySelector(".portal-column1");
 const column2 = document.querySelector(".portal-column2");
-const email = links.querySelector("span:last-child");
+const email = links.querySelector("textarea:last-child");
 
 function handleClickLinkBtn() {
   links.classList.toggle("links__linkbtn");
